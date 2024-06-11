7f2dbb0f0cbc9bc385acc5895bf8f4b6302b8e2c
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-06-11 18:24:21 +0000

    correction de bug

--- script.js
+++ script.js
@@ -34,16 +34,16 @@ document.addEventListener('DOMContentLoaded', function() {
         let alertMessage = '';
         let alertClass = '';
         let alertIcon = '';
-        if (score === 15) {
-            alertMessage = `Félicitations ! Vous avez obtenu tous les points (${score}/15 points).`;
+        if (score === 20) {
+            alertMessage = `Félicitations ! Votre score est (${score}/20 points).`;
             alertClass = 'alert alert-success';
             alertIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
-        } else if (score >= 7.5) {
-            alertMessage = `Bien joué ! Vous avez la moyenne (${score}/15 points).`;
+        } else if (score >= 10) {
+            alertMessage = `Bien joué ! Votre score est (${score}/20 points).`;
             alertClass = 'alert';
             alertIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
         } else {
-            alertMessage = `Dommage, vous avez une mauvaise note (${score}/15 points).`;
+            alertMessage = `Dommage, Votre score est (${score}/20 points).`;
             alertClass = 'alert alert-error';
             alertIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
         }
